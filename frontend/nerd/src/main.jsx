15fa0af223ead402a85b8c1d19e27e0be2c53e6e
ScriptKarma <cauabdecol@gmail.com>
2025-09-10 14:51:07 -0300

    se tive algo arrume

--- frontend/nerd/src/main.jsx
+++ frontend/nerd/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Perfil from "./perfil";
+import Navbar from "./navbar";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/perfil" element={<Perfil />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
